cc19dee2ded16142ec60bfdc7394b8b4fa3e92b1
ArdTae <137835319+ArdTae@users.noreply.github.com>
2023-07-29 01:07:03 +0500

    Installation_bag
    Installation package

---
+++ installation_bag.sh
@@ -0,0 +1,11 @@
+pkg install python
+pkg install python2
+pkg install git
+pkg install python-pip
+pip install os
+pip install datetime
+pip install time
+pip install threading
+pip install psutil
+pip install platform
+pip install vk-api
