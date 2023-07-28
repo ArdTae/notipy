fd0a4caa1977d65f2d14f023007b02449453cf69
ArdTae <137835319+ArdTae@users.noreply.github.com>
2023-07-29 01:05:54 +0500

    banners
    Banner files used

---
+++ banners.py
@@ -0,0 +1,13 @@
+mailba = " ╭━╮╭━╮╱╱╱╱╭━━╮╭╮\n ┃┃╰╯┃┃╱╱╱╱╰┫┣╯┃┃\n ┃╭╮╭╮┃╭━━╮╱┃┃╱┃┃\n ┃┃┃┃┃┃┃╭╮┃╱┃┃╱┃┃\n ┃┃┃┃┃┃┃╭╮┃╭┫┣╮┃╰╮\n ╰╯╰╯╰╯╰╯╰╯╰━━╯╰━╯"
+notipy_banner = " ███╗░░██╗░█████╗░████████╗██╗██████╗░██╗░░░██╗ |\n ████╗░██║██╔══██╗╚══██╔══╝██║██╔══██╗╚██╗░██╔╝ |\n ██╔██╗██║██║░░██║░░░██║░░░██║██████╔╝░╚████╔╝░ |\n ██║╚████║██║░░██║░░░██║░░░██║██╔═══╝░░░╚██╔╝░░ |\n ██║░╚███║╚█████╔╝░░░██║░░░██║██║░░░░░░░░██║░░░ |\n ╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝╚═╝░░░░░░░░╚═╝░░░ |"
+enter_banner = "                                                   |"
+
+
+htimep = "After an hour, you need to enter [/turn]"
+hpost = "Remaining time in seconds: "
+up10 = "Time is up, wait another 10 seconds..."
+seth = "These settings are responsible for actions after the timer"
+setm = "[vk] Vk, [tg] Telegram, [yt] YouTube [delf] Delete files [ma] Mail [all] All commands"
+standvkmess = "This post was created by the notipy program. For more information, please contact the account owner"
+other_banner = "Enter the amount of time (in seconds): "
+mail_banner = "[se] Send mail [del] Delete account [cl] Clear all mails"
